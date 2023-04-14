44e6de5b8bd6a39e6e3ab1521cd498feaaa0f1a0
india <india@DESKTOP-RPCT66H>
2023-04-14 15:43:09 +0530

    akash code is running successful

--- src/main/java/Akash/fblogin.java
+++ src/main/java/Akash/fblogin.java
@@ -5,14 +5,17 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class fblogin {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		WebDriver driver = new ChromeDriver();
+		ChromeOptions option= new ChromeOptions();
+		option.addArguments("--remote-allow-origins=*");
+		
+		WebDriver driver = new ChromeDriver(option);
 		driver.manage().window().maximize();
 		
-		driver.get("www.facebook.com/");
+		driver.get("https://www.facebook.com/login/");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		
 		driver.findElement(By.id("email")).sendKeys("Akash Swamy");
